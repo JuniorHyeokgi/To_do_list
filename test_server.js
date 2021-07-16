e1eb499f631f2d198e3624b40c52f32c1066fc88
Hyeokgi <chinup1004@naver.com>
2021-07-17 04:14:57 +0900

    Template Engine of pug, ejs

--- test_server.js
+++ test_server.js
@@ -1,34 +1,13 @@
 var express = require('express');
 var app = express();
-var boardRouter = require('./router/board.js');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  var template =
-  `<!DOCTYPE html>
-  <html>
-    <body>
-      <form action='/login' method='post'>
-        ID : <input type='text' name='id'><br>
-        PWD : <input type='password' name='pwd'><br>
-        <input type='submit' value='login'>
-      </form>  
-    </body>
-  </html>`;
-  res.writeHead(200, {'ContentType':'text/html'});
-  res.write(template);
-  res.end();
+  res.render('template.ejs', {title:'EJS : EXPRESS TEMPLATE ENGINE'});
 });
 
-app.post('/login', function(req, res) {
-  var body = req.body;
-  console.log(body);
-
-  res.send("ID : " + body.id + " / PWD : " + body.pwd);
-})
-
-app.use('/board', boardRouter);
-
-app.listen(3000, () => console.log('Server listen on port 3000...'));
+app.listen(3000, function() {
+  console.log('Server running on port 3000...');
+});
