c87c81f10b095425c3be57e32b183ef2d4cfc50f
Hyeokgi <chinup1004@naver.com>
2021-07-18 18:04:42 +0900

    mysql

---
+++ config/database.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var db_info = {
+    host: 'localhost',
+    port: '3306',
+    user: 'user_name',
+    password: 'password',
+    database: 'db_name'
+}
+
+module.exports = {
+    init: function() {
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn) {
+        conn.connect(function(err) {
+            if(err) console.err('mysql connection err : ' + err);
+            else console.log('mysql is connected successfully!');
+        });
+    }
+}
